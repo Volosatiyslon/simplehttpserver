e31deae5dec5285eed2b8a2205738ec07361cf85
Oleg Borisov <borisov@group-ib.com>
2022-04-12 20:55:35 +0300

    now using template

--- main.go
+++ main.go
@@ -3,172 +3,64 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"log"
-	"mime"
 	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
 )
-
-type route struct{
-	fName 	string
-	fMime	string
-}
-
-const MAXSIZE = 1024*1024*1024 //1G
-
-func createIndex(fileList []route, t string, addUpload bool) (error){
-	var template string
-	os.Remove("index.html")
-	template_file, err:=os.ReadFile(t)
-	if err == nil && strings.Index(string(template_file), "##REPLACE##")>0 {
-		template = string(template_file)
-	}else{
-		template = "<html><body><div>this index file is auto generated after start</div><p></p>##REPLACE##</body></html>"
-	}
-	var content string
-	for _,f := range fileList{
-		if f.fName == ""{
-			continue
-		}
-		content+=fmt.Sprintf("<p><a href=\"./%v\">%v</a></p>", f.fName, f.fName)
-	}
-	if addUpload{
-		content += "<form id=\"form\" enctype=\"multipart/form-data\" action=\"/upload\" method=\"POST\">"
-		content += "<input class=\"input file-input\" type=\"file\" name=\"file\" />"
-		// content += "<input class=\"input file-input\" type=\"file\" name=\"file\" multiple />"
-		content += "<button class=\"button\" type=\"submit\">Submit</button> </form>"
-	}
-	file := strings.Replace(template,"##REPLACE##", content, 1) 
-	err = os.WriteFile("index.html", []byte(file), 0777)
-	if err != nil{
-		log.Fatalf("err during creating index: %v", err)
-	}
-	return nil
-}
-
-func filteredFileList(exept string) ([]route, bool, error){
-	fileList, err := os.ReadDir(".")
-	if err != nil{
-		return nil, false, fmt.Errorf("err during reading dir: %v", err)
-	}
-	needIndex := true
-	routeList := make([]route,0, len(fileList))
-	for _,f := range fileList{
-		if f.IsDir(){
-			continue
-		}
-		if f.Name()[0]=='.'{
-			continue
-		}
-		if f.Name() == exept{
-			continue
-		}
-		if f.Name() == "index.html"{
-			needIndex = false
-		}
-		ext := ""
-		s:=strings.Split(f.Name(), ".")
-		if ext =  mime.TypeByExtension(fmt.Sprintf(".%v",s[len(s)-1])); ext == ""{
-			ext = "application/octet-stream"
-		}
-		routeList = append(routeList, route{
-			fName: f.Name(),
-			fMime: ext,
-		})
-	}
-	return routeList,needIndex, nil
-}
-
-func create_handler(f route)(handler func(w http.ResponseWriter, r *http.Request)){
-	handler = func(w http.ResponseWriter, r *http.Request){
-		if r.Method != "GET"{
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(400)
-			w.Write([]byte("wrong method, only GET allowed"))
-			log.Printf("%v, %v, %v, %v, %v", r.RemoteAddr, r.Method, r.URL.Path, "400", w.Header())
-		}
-		content, err := os.ReadFile(f.fName)
-		if err != nil{
-			w.Header().Set("Content-Type", "text/html")
-			w.WriteHeader(400)
-			w.Write([]byte(fmt.Sprintf("error during reading file: %v", err)))
-		}
-		w.Header().Set("Content-Type", f.fMime)
-		w.Write(content)
-		log.Printf("%v, %v, %v, %v, %v", r.RemoteAddr, r.Method, r.URL.Path, "200", w.Header())
-
-	}
-	return
-}
-
-func uploadHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	r.Body = http.MaxBytesReader(w, r.Body, MAXSIZE)
-	file, fileHeader, err := r.FormFile("file")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-	dst, err := os.Create(fmt.Sprintf("./%d%s.uploaded", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer dst.Close()
-	_, err = io.Copy(dst, file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	http.Redirect(w, r, "/index.html", http.StatusMovedPermanently)
-
-}
 
 func main(){
 	var address string
-	var template string
-	indexRewrite := flag.Bool("r",false,"check to rewrite index file")
-	enableUpload := flag.Bool("u",false,"enable upload handler")
+	uploadEnabled := flag.Bool("u",false,"enable upload handler")
 	flag.StringVar(&address, "a", "127.0.0.1:8080", "Specify address. Default is \"127.0.0.1:8080\"")
-	flag.StringVar(&template, "t", "", "Specify index file template, default is empty (index will be selfgenerated)")
-	flag.Parse()	
-	path_list := strings.Split(os.Args[0], "/")
-	exec_file := path_list[len(path_list)-1]
-	routes, indexNotExist, err := filteredFileList(exec_file)
+	flag.Parse()
+	s := Server{
+		UploadEnabled: *uploadEnabled,
+	}
+	err := s.BuildRoutes()
 	if err != nil{
-		log.Fatalf("error while reading dir: %v", err)
+		log.Fatalf("error during building routes: %v", err)
 	}
-	if indexNotExist || (*indexRewrite){
-			log.Print("creating index.html")
-			createIndex(routes, template, (*enableUpload))
-		}
-	
-	for _, f := range routes{
-		if f.fName == "index.html"{
-				continue
-			}
-		http.HandleFunc(fmt.Sprintf("/%v", f.fName), create_handler(f))
-		// log.Printf("%+v", f)
-		}
-	indexhandler := create_handler(route{fName: "index.html",fMime: "text/html"})
-	http.HandleFunc("/index.html", indexhandler )
-	http.HandleFunc("/", indexhandler)
-	if (*enableUpload){
-		http.HandleFunc("/upload", uploadHandler)
+	for _, f := range s.Routes{
+		http.HandleFunc(fmt.Sprintf("/%v", f.Name), f.Create_handler())	
 	}
+	if s.UploadEnabled{
+		http.HandleFunc("/upload", s.UploadHandler)
+	}
+	http.HandleFunc("/index.html", s.IndexHandler )
+	http.HandleFunc("/", s.IndexHandler)
 	log.Printf("started on %v", address)
 	err = http.ListenAndServe(address, nil)
 	if err != nil{
 		log.Fatalf("error during serve http: %v", err)
 	}
+	// path_list := strings.Split(os.Args[0], "/")
+	// exec_file := path_list[len(path_list)-1]
+	// routes, indexNotExist, err := filteredFileList(exec_file)
+	// if err != nil{
+	// 	log.Fatalf("error while reading dir: %v", err)
+	// }
+	// if indexNotExist || (*indexRewrite){
+	// 		log.Print("creating index.html")
+	// 		createIndex(routes, template, (*enableUpload))
+	// 	}
+	
+	// for _, f := range routes{
+	// 	if f.fName == "index.html"{
+	// 			continue
+	// 		}
+	// 	http.HandleFunc(fmt.Sprintf("/%v", f.fName), create_handler(f))
+	// 	// log.Printf("%+v", f)
+	// 	}
+	// indexhandler := create_handler(route{fName: "index.html",fMime: "text/html"})
+	// http.HandleFunc("/index.html", indexhandler )
+	// http.HandleFunc("/", indexhandler)
+	// if (*enableUpload){
+	// 	http.HandleFunc("/upload", uploadHandler)
+	// }
+	// log.Printf("started on %v", address)
+	// err = http.ListenAndServe(address, nil)
+	// if err != nil{
+	// 	log.Fatalf("error during serve http: %v", err)
+	// }
 }
 
 /*
---
+++ server.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+var tSimple = `
+	<html><body>
+	<div>this index file is auto generated after start</div><p></p>
+	{{range .Routes}}
+	<p><a href="./{{ .Name }}">/{{ .Name }}</a></p>
+	{{ end }}
+	</body></html>
+	`
+var tWithUpload = `
+	<html><body>
+	<div>this index file is auto generated after start</div><p></p>
+	{{range .Routes}}
+	<p><a href="./{{ .Name }}">/{{ .Name }}</a></p>
+	{{ end }}
+	<form id="form" enctype="multipart/form-data" action="/upload" method="POST">
+		<input class="input file-input" type="file" name="file">
+		<button class="button" type="submit">Submit</button> 
+	</form>
+	</body></html>
+	`
+
+const MAXSIZE = 1024*1024*1024 //1G
+
+type Route struct{
+	Name 	string
+	fMime	string
+}
+	
+type Server struct {
+	Routes 			[]Route
+	UploadEnabled 	bool
+}
+
+func (f Route) Create_handler() (handler func(w http.ResponseWriter, r *http.Request)){
+	handler = func(w http.ResponseWriter, r *http.Request){
+		if r.Method != "GET"{
+			w.Header().Set("Content-Type", "text/html")
+			w.WriteHeader(400)
+			w.Write([]byte("wrong method, only GET allowed"))
+			log.Printf("%v, %v, %v, %v, %v", r.RemoteAddr, r.Method, r.URL.Path, "400", w.Header())
+		}
+		content, err := os.ReadFile(f.Name)
+		if err != nil{
+			w.Header().Set("Content-Type", "text/html")
+			w.WriteHeader(400)
+			w.Write([]byte(fmt.Sprintf("error during reading file: %v", err)))
+		}
+		w.Header().Set("Content-Type", f.fMime)
+		w.Write(content)
+		log.Printf("%v, %v, %v, %v, %v", r.RemoteAddr, r.Method, r.URL.Path, "200", w.Header())
+
+	}
+	return handler
+}
+func (s *Server) GetRoute (filename string) (Route, error){
+	for _,r := range s.Routes{
+		if r.Name == filename{
+			return r, nil
+		}
+	}
+	return Route{}, fmt.Errorf("there is no such file")
+}
+
+func (s *Server) BuildRoutes()(error){
+	fileList, err := os.ReadDir(".")
+	if err != nil {
+		return fmt.Errorf("cant read directory: %v", err)
+	}
+	s.Routes = make([]Route, 0, len(fileList))
+	for _,f := range fileList{
+		if f.IsDir(){
+			continue
+		}
+		if f.Name()[0]=='.'{
+			continue
+		}
+		ext := ""
+		nameParts :=strings.Split(f.Name(), ".")
+		if ext =  mime.TypeByExtension(fmt.Sprintf(".%v",nameParts [len(nameParts )-1])); ext == ""{
+			ext = "application/octet-stream"
+		}
+		
+		s.Routes = append(s.Routes, Route{
+			Name: f.Name(),
+			fMime: ext,
+		})
+	}
+	return nil
+}
+
+func (s *Server) IndexHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != "GET"{
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(400)
+		w.Write([]byte("wrong method, only GET allowed"))
+		log.Printf("%v, %v, %v, %v, %v", r.RemoteAddr, r.Method, r.URL.Path, "400", w.Header())
+		return
+	}
+	content,err := s.ReturnIndex()
+	if err != nil{
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(500)
+		w.Write([]byte("internal server error"))
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	w.Write(content)
+	log.Printf("%v, %v, %v, %v, %v", r.RemoteAddr, r.Method, r.URL.Path, "200", w.Header())
+	return
+}
+
+func (s *Server) UploadHandler(w http.ResponseWriter, r *http.Request){
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	r.Body = http.MaxBytesReader(w, r.Body, MAXSIZE)
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer file.Close()
+	newName := fmt.Sprintf("%d%s.uploaded", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename))
+	dst, err := os.Create(fmt.Sprintf("./%v",newName))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dst.Close()
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("newname: %v",newName)
+	s.BuildRoutes()
+	for _,v := range s.Routes{
+		log.Printf("file: %v", v.Name)
+	}
+	route, err := s.GetRoute(newName)
+	log.Printf("%+v",route )
+	if err!=nil{
+		log.Printf("err: %v",err)
+		w.Header().Set("Content-Type", "text/html")
+		w.WriteHeader(500)
+		w.Write([]byte("internal server error"))
+		return
+	}
+	http.HandleFunc(fmt.Sprintf("/%v", newName), route.Create_handler())
+	http.Redirect(w, r, "/index.html", http.StatusMovedPermanently)
+}
+
+func (s *Server) ReturnIndex()([]byte, error){
+	t := template.New("index")
+	var err error
+	if s.UploadEnabled{
+		t, err  = t.Parse(tWithUpload)
+	}else{
+		t, err  = t.Parse(tSimple)
+	}
+	if err != nil {
+		return nil, err
+	}
+	var b bytes.Buffer
+	err = t.Execute(&b,s)
+	return b.Bytes(), err
+}
